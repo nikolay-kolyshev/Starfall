2b8d0a2c2f5e8445f4c9252b2347ff986a58ffec
nikolay-kolyshev <sword.of.destiny2001@mail.ru>
2021-02-15 10:55:28 +0300

    create nextJS app

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Starfall</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+      </main>
+    </div>
+  )
+}
+
+export default Home
